775b83e746e421c75af50c17f19a5a8eda2c4472
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-02-27 22:19:17 +0600

    updated styles & made responsive

--- src/js/script.js
+++ src/js/script.js
@@ -4,21 +4,10 @@ const verticalContentH1 = document.querySelector(".timeline-heading");
 const verticalContentPara =document.querySelector(".timeline-para");
 const Bgimage= document.querySelector(".carousel-bg");
 
-// const btResponse=()=>{
-//     let classContain;
-//     for(let i=0; i<timelineBTN.length; i++){
-//         timelineBTN[i].addEventListener("click",()=>{
-//             console.log("btn clicked from line 7");
-//         });
-
-//     }
-// }
 
 
-// vertical counter
-
-
-
+// TIMELINE SLIDER DATA
+// ====================================
 const DataSlider = {
     sliderOne: {
         h1: "TIMELINE COUNTER",
@@ -42,52 +31,8 @@ const DataSlider = {
     }
 }
 
-// // a function for content changing handler
-// const contentChanger = (slider) => {
-//     // get data from the object according to condition  
-//     if (slider === 1) {
-//         let heading = DataSlider.sliderOne.h1;
-//         let para = DataSlider.sliderOne.p;
-//         let bgImage = DataSlider.sliderOne.bgImagePath;
-//         verticalContentH1.textContent = heading;
-//         verticalContentPara.textContent = para;
-//         verticalCarouselContainer.style.background = 'url(' + bgImage + ')';
-//     }
-//     else if (slider === 2) {
-//         let heading = DataSlider.sliderTwo.h1;
-//         let para = DataSlider.sliderTwo.p;
-//         let bgImage = DataSlider.sliderTwo.bgImagePath;
-//         verticalContentH1.textContent = heading;
-//         verticalContentPara.textContent = para;
-//         verticalCarouselContainer.style.background = 'url(' + bgImage + ')';
-//     }
-//     else if (slider === 3) {
-//         let heading = DataSlider.sliderThree.h1;
-//         let para = DataSlider.sliderThree.p;
-//         let bgImage = DataSlider.sliderThree.bgImagePath;
-//         verticalContentH1.textContent = heading;
-//         verticalContentPara.textContent = para;
-//         verticalCarouselContainer.style.background = 'url(' + bgImage + ')';
-//     }
-//     else if (slider === 4) {
-//         let heading = DataSlider.sliderFour.h1;
-//         let para = DataSlider.sliderFour.p;
-//         let bgImage = DataSlider.sliderFour.bgImagePath;
-//         verticalContentH1.textContent = heading;
-//         verticalContentPara.textContent = para;
-//         verticalCarouselContainer.style.background = 'url(' + bgImage + ')';
-//     }
-
-//     // verticalContentH1.textContent=heading;
-//     // verticalContentPara.textContent =para;
-// }
-
-
-
-
-
-
-// timeline button color function
+// TIMELINE SLIDER FUNCTION
+// ==============================================================
 for (let i = 0; i < timelineBTN.length; i++) {
     timelineBTN[i].addEventListener("click", () => {
         let ListClass = timelineBTN[i].classList;
@@ -95,9 +40,14 @@ for (let i = 0; i < timelineBTN.length; i++) {
             // COLOR FUNCTION
             filler.style.height = "18%";
             timelineBTN[0].classList.add("bt-active");
+            timelineBTN[0].classList.remove("bt-white");
+
             timelineBTN[1].classList.remove("bt-active");
+            timelineBTN[1].classList.remove("bt-white");
             timelineBTN[2].classList.remove("bt-active");
+            timelineBTN[2].classList.remove("bt-white");
             timelineBTN[3].classList.remove("bt-active");
+            timelineBTN[3].classList.remove("bt-white");
 
             // DATA FETCHING AND RENDERING
             let heading = DataSlider.sliderOne.h1;
@@ -111,9 +61,12 @@ for (let i = 0; i < timelineBTN.length; i++) {
             // COLOR FUNCTION
             filler.style.height = "36%";
             timelineBTN[0].classList.remove("bt-active");
+            timelineBTN[0].classList.add("bt-white");
             timelineBTN[1].classList.add("bt-active");
             timelineBTN[2].classList.remove("bt-active");
+            timelineBTN[2].classList.remove("bt-white");
             timelineBTN[3].classList.remove("bt-active");
+            timelineBTN[3].classList.remove("bt-white");
 
             // DATA FETCHING AND RENDERING
             let heading = DataSlider.sliderTwo.h1;
@@ -128,9 +81,12 @@ for (let i = 0; i < timelineBTN.length; i++) {
             // COLOR FUNCTION
             filler.style.height = "54%";
             timelineBTN[0].classList.remove("bt-active");
+            timelineBTN[0].classList.add("bt-white");
             timelineBTN[1].classList.remove("bt-active");
+            timelineBTN[1].classList.add("bt-white");
             timelineBTN[2].classList.add("bt-active");
             timelineBTN[3].classList.remove("bt-active");
+            timelineBTN[3].classList.remove("bt-white");
 
             // DATA FETCHING AND RENDERING
             let heading = DataSlider.sliderThree.h1;
@@ -144,7 +100,9 @@ for (let i = 0; i < timelineBTN.length; i++) {
             // COLOR FUNCTION
             filler.style.height = "73%";
             timelineBTN[0].classList.remove("bt-active");
+            timelineBTN[0].classList.add("bt-white");
             timelineBTN[1].classList.remove("bt-active");
+            timelineBTN[2].classList.add("bt-white");
             timelineBTN[2].classList.remove("bt-active");
             timelineBTN[3].classList.add("bt-active");
 
@@ -158,3 +116,10 @@ for (let i = 0; i < timelineBTN.length; i++) {
         }
     })
 }
+
+
+
+
+
+
+
