882faafae6d0044c4422c96dc8d953aa076971bb
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-03-01 00:37:20 +0600

    fixed bugs, removed unsed code

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,9 @@ const verticalContentH1 = document.querySelector(".timeline-heading");
 const verticalContentPara = document.querySelector(".timeline-para");
 const Bgimage = document.querySelectorAll(".carousel-bg");
 const TimelineContainer = document.querySelector(".timeline-carousel");
-const timeLineTextContent= document.querySelector(".timeline-text-container");
-const timeLineMenu= document.querySelector(".timeline-menu");
-const timeLineBtnContainer= document.querySelector(".timeline-btn-container");
+const timeLineTextContent = document.querySelector(".timeline-text-container");
+const timeLineMenu = document.querySelector(".timeline-menu");
+const timeLineBtnContainer = document.querySelector(".timeline-btn-container");
 
 
 // TIMELINE SLIDER DATA
@@ -38,27 +38,22 @@ const DataSlider = {
 // ==============================================================
 const SwitchSlider = {
     sliderOne: () => {
-        // COLOR FUNCTION
         filler.style.height = "18%";
         timelineBTN[0].classList.add("bt-active");
-        // timelineBTN[0].classList.remove("bt-white");
-
         timelineBTN[1].classList.remove("bt-active");
-        // timelineBTN[1].classList.remove("bt-white");
         timelineBTN[2].classList.remove("bt-active");
-        // timelineBTN[2].classList.remove("bt-white");
         timelineBTN[3].classList.remove("bt-active");
-        // timelineBTN[3].classList.remove("bt-white");
 
-        // DATA FETCHING AND RENDERING
+
+        // DATA FETCHING FROM OBJECT(DataSlider) AND REPLACING
         let heading = DataSlider.sliderOne.h1;
         let para = DataSlider.sliderOne.p;
         verticalContentH1.textContent = heading;
         verticalContentPara.textContent = para;
-        // let bgImage = DataSlider.sliderOne.bgImagePath;
-        // Bgimage.src = bgImage;
 
-        // IMAGE BG OPACITY
+
+
+        // IMAGE OPACITY HANDLER
         Bgimage[0].classList.add("op-1");
         Bgimage[0].classList.remove("op-0");
 
@@ -72,26 +67,23 @@ const SwitchSlider = {
         Bgimage[3].classList.remove("op-1");
     },
     sliderTwo: () => {
-        // COLOR FUNCTION
         filler.style.height = "36%";
         timelineBTN[0].classList.remove("bt-active");
-        // timelineBTN[0].classList.add("bt-white");
         timelineBTN[1].classList.add("bt-active");
         timelineBTN[2].classList.remove("bt-active");
-        // timelineBTN[2].classList.remove("bt-white");
         timelineBTN[3].classList.remove("bt-active");
-        // timelineBTN[3].classList.remove("bt-white");
 
-        // DATA FETCHING AND RENDERING
+
+       // DATA FETCHING FROM OBJECT(DataSlider) AND REPLACING
         let heading = DataSlider.sliderTwo.h1;
         let para = DataSlider.sliderTwo.p;
-        // let bgImage = DataSlider.sliderTwo.bgImagePath;
+
         verticalContentH1.textContent = heading;
         verticalContentPara.textContent = para;
-        // Bgimage.src = bgImage;
 
 
-        // IMAGE BG OPACITY
+
+        // IMAGE OPACITY HANDLER
         Bgimage[0].classList.add("op-0");
         Bgimage[0].classList.remove("op-1");
 
@@ -105,25 +97,22 @@ const SwitchSlider = {
         Bgimage[3].classList.remove("op-1");
     },
     sliderThree: () => {
-        // COLOR FUNCTION
         filler.style.height = "54%";
         timelineBTN[0].classList.remove("bt-active");
-        // timelineBTN[0].classList.add("bt-white");
         timelineBTN[1].classList.remove("bt-active");
-        // timelineBTN[1].classList.add("bt-white");
         timelineBTN[2].classList.add("bt-active");
         timelineBTN[3].classList.remove("bt-active");
-        // timelineBTN[3].classList.remove("bt-white");
 
-        // DATA FETCHING AND RENDERING
+
+        // DATA FETCHING FROM OBJECT(DataSlider) AND REPLACING
         let heading = DataSlider.sliderThree.h1;
         let para = DataSlider.sliderThree.p;
-        // let bgImage = DataSlider.sliderThree.bgImagePath;
+
         verticalContentH1.textContent = heading;
         verticalContentPara.textContent = para;
-        // Bgimage.src = bgImage;
 
-        // IMAGE BG OPACITY
+
+        // IMAGE OPACITY HANDLER
         Bgimage[0].classList.add("op-0");
         Bgimage[0].classList.remove("op-1");
 
@@ -137,25 +126,19 @@ const SwitchSlider = {
         Bgimage[3].classList.remove("op-1");
     },
     sliderFour: () => {
-        // COLOR FUNCTION
         filler.style.height = "73%";
         timelineBTN[0].classList.remove("bt-active");
-        // timelineBTN[0].classList.add("bt-white");
         timelineBTN[1].classList.remove("bt-active");
-        // timelineBTN[2].classList.add("bt-white");
         timelineBTN[2].classList.remove("bt-active");
         timelineBTN[3].classList.add("bt-active");
 
-        // DATA FETCHING AND RENDERING
+        // DATA FETCHING FROM OBJECT(DataSlider) AND REPLACING
         let heading = DataSlider.sliderFour.h1;
         let para = DataSlider.sliderFour.p;
-        // let bgImage = DataSlider.sliderFour.bgImagePath;
         verticalContentH1.textContent = heading;
         verticalContentPara.textContent = para;
-        // Bgimage.src = bgImage;
 
-
-        // IMAGE BG OPACITY
+        // IMAGE OPACITY HANDLER
         Bgimage[0].classList.add("op-0");
         Bgimage[0].classList.remove("op-1");
 
@@ -171,13 +154,9 @@ const SwitchSlider = {
 }
 
 
-
-
 // CLICK EVENTS FOR TIMELINE
 for (let i = 0; i < timelineBTN.length; i++) {
     timelineBTN[i].addEventListener("click", () => {
-        // 
-        // TimelineFunction();
         let ListClass = timelineBTN[i].classList;
         if (ListClass.contains("bt-1")) {
             SwitchSlider.sliderOne();
@@ -197,82 +176,75 @@ for (let i = 0; i < timelineBTN.length; i++) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // TIMELINE SCROLL BEHAVIOUR
 let percentage;
 let scrollingStatus;
+let ActivateScroll = false;
+
+
+// THIS FUNCTION TELLS IF USER SCROLLED UP OR DOWN
+window.addEventListener('wheel', function (event) {
+    var delta = Math.sign(event.deltaY);
+    if (delta > 0) {
+        scrollingStatus = "down";
+
+    } else if (delta < 0) {
+        scrollingStatus = "up";
+    }
+});
+
 
 // MAIN FUNCTION OF SCROLL BEHAVIOUR
 const ScrollingEffect = () => {
     let y = timeLineBtnContainer.clientHeight;
 
-    // AFTER MOUSEOVER IF I SCROLL -GET WHEN I START SCROLLING
+    // AFTER MOUSEOVER IF I SCROLL, GET DATA ABOUT- WHEN I START SCROLLING
     let timer = null;
     window.addEventListener('wheel', function () {
         let x = filler.clientHeight;
         percentage = Math.floor((x / y) * 100);
 
 
-        // console.log("function is scrolling");
+        // THIS if statement and timer(setTimeout) functions run some code when user stopped scrolling
+        // this is important for preventing multiple responsve from the events
         if (timer !== null) {
             clearTimeout(timer);
         }
         timer = setTimeout(function () {
             // if the scrolling stopped - this function will be executed
-            // console.log("function stopped");
             if (scrollingStatus == "down") {
                 if (percentage == 17) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderTwo();
                 }
                 else if (percentage == 36) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderThree();
                 }
                 else if (percentage == 53) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderFour();
                 }
                 else if (percentage == 72) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderOne();
                 }
             }
             else if (scrollingStatus == "up") {
                 if (percentage == 17) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderFour();
                 }
                 else if (percentage == 36) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderOne();
                 }
                 else if (percentage == 53) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderTwo();
                 }
                 else if (percentage == 72) {
-                    // console.log("percentage" + percentage);
                     SwitchSlider.sliderThree();
                 }
             }
         }, 350);
+
     }, false);
+
 }
-
-
-
 
 timeLineTextContent.addEventListener("mouseenter", () => {
     // CALLING THE MAIN FUNCTION
@@ -280,52 +252,16 @@ timeLineTextContent.addEventListener("mouseenter", () => {
     document.body.style.overflowY = "hidden";
 });
 
-timeLineTextContent.addEventListener("mouseleave",()=>{
+timeLineTextContent.addEventListener("mouseleave", () => {
     document.body.style.overflowY = "auto";
 });
 
-timeLineMenu.addEventListener("mouseenter", () => {
-    // CALLING THE MAIN FUNCTION
-    ScrollingEffect();
-    document.body.style.overflowY = "hidden";
-});
+// timeLineMenu.addEventListener("mouseenter", () => {
+//     // CALLING THE MAIN FUNCTION
+//     ScrollingEffect();
+//     document.body.style.overflowY = "hidden";
+// });
 
-timeLineMenu.addEventListener("mouseleave", () => {
-    document.body.style.overflowY = "auto";
-});
-
-
-
-
-
-// I have read ==== if it is scrolling down or up
-// done reading the scroll up or down
-// next task is take multiple input into one 
-// OR TAKE ONLY ONE INPUT
-const checkWheelInput = () => {
-    window.addEventListener('wheel', function (event) {
-        var delta = Math.sign(event.deltaY);
-        if (delta > 0) {
-            // console.log('Scrolling down');
-            scrollingStatus = "down";
-
-        } else if (delta < 0) {
-            // console.log('Scrolling up');
-            scrollingStatus = "up";
-        }
-    });
-}
-
-
-// THE FUNCTION NEEDED TO CALL ITSELF 
-//OTHERWISE THE FIRST SLIDE ANIMATION DOESN'T WORK
-checkWheelInput();
-
-
-
-
-
-
-
-
-
+// timeLineMenu.addEventListener("mouseleave", () => {
+//     document.body.style.overflowY = "auto";
+// });
